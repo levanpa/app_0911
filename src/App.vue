88b6071c4b5b277123174b7c3c04c7940870e49c
Kai Pa <37103702+levanpa@users.noreply.github.com>
2021-09-11 16:04:55 +0700

    [upd] install scss/sass

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,14 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+  .hello
+    background-color: #fee
 </style>
