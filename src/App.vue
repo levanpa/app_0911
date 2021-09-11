5ec8d22ca5c5ab1f0df78fbe13193af3532cf014
Kai Pa <37103702+levanpa@users.noreply.github.com>
2021-09-11 16:13:26 +0700

    [upd] setup global variables and mixxins

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,15 @@ export default {
 
 <style lang="sass">
 #app
-  font-family: Avenir, Helvetica, Arial, sans-serif
+  font-family: $font-serif
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
   text-align: center
-  color: #2c3e50
+  color: $font-color
   margin-top: 60px
   .hello
     background-color: #fee
++desktop
+  body
+    font-size: $font-size
 </style>
