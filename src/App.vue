6515485b09ab8827b64e2c336c0643ff2a55158f
Kai Pa <37103702+levanpa@users.noreply.github.com>
2021-09-11 16:30:19 +0700

    [upd] setup base sass, home page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template lang="pug">
-img(alt="Vue logo" src="./assets/logo.png")
-HelloWorld(msg="Welcome to Your Vue.js App")
+Home
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
 
-<style lang="sass">
-#app
-  font-family: $font-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: $font-color
-  margin-top: 60px
-  .hello
-    background-color: #fee
-+desktop
-  body
-    font-size: $font-size
+<style>
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+.home-component-wrapper
+  img(alt="Vue logo", src="../assets/logo.png")
+  HelloWorld(msg="Welcome to Your Vue.js App")
+</template>
+
+<script>
+import HelloWorld from './HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="sass">
+@import "../sass/pages/home.sass"
+</style>
