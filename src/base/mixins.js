3c6236bfc1e8bc1a396b3b65a9e8198c7812da1d
Kai Pa <37103702+levanpa@users.noreply.github.com>
2021-09-29 22:21:25 +0700

    [upd] seperate router and mixins

---
+++ src/base/mixins.js
@@ -0,0 +1,5 @@
+export default {
+  methods: {
+    log: (...data) => console.log(...data)
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,44 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import * as VueRouter from 'vue-router';
-import Home from "./components/Home.vue";
-import Products from "./components/ProductList.vue";
-import ProductDetail from "./components/ProductDetail.vue";
-import Login from "./components/parts/Login.vue";
-import Signup from "./components/parts/Signup.vue";
-import ForgotPass from "./components/parts/ForgotPass.vue";
-import Contact from "./components/Contact.vue";
-import Account from "./components/Account.vue";
+import router from './base/router.js';
+import mixins from './base/mixins.js';
 
-const routes = [
-  { path: '/', component: Home },
-  { path: '/products', component: Products },
-  { path: '/product-detail', component: ProductDetail },
-  { path: '/login', component: Login },
-  { path: '/signup', component: Signup },
-  { path: '/forgot-password', component: ForgotPass },
-  { path: '/contact', component: Contact },
-  { path: '/account', component: Account },
-];
-
-const router = VueRouter.createRouter({
-  history: VueRouter.createWebHashHistory(),
-  routes,
-});
-
-router.beforeEach((to, from, next) => {
-  window.scrollTo(0, 0);
-  setTimeout(() => next(), 100);
-});
-
-// init vue app
 const vueApp = createApp(App);
 
-vueApp.mixin({
-  methods: {
-    log: (...data) => console.log(...data)
-  }
-});
-
+vueApp.mixin(mixins);
 vueApp.use(router);
 vueApp.mount('#app');
