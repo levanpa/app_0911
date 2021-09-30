ea50498773673f689ea05fdd6d1ed65529d9ebb0
Kai Pa <37103702+levanpa@users.noreply.github.com>
2021-09-30 21:34:56 +0700

    [upd] init filters

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from '@/App.vue';
 import router from '@/base/router.js';
 import mixins from '@/base/mixins.js';
 import store from '@/base/store.js';
+import filters from '@/base/filters.js';
 
 const vueApp = createApp(App);
 
+vueApp.config.globalProperties.$filters = filters;
 vueApp.mixin(mixins)
   .use(router)
   .use(store)
