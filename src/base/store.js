0c53daeac88a048e6609cbae8101d2678fc094e9
Kai Pa <37103702+levanpa@users.noreply.github.com>
2021-09-30 21:33:51 +0700

    [upd] create store

---
+++ src/base/store.js
@@ -0,0 +1,15 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+  state() {
+    return {
+      testData: 'SUCCESS',
+    }
+  },
+  mutations: {
+  },
+  actions: {
+  },
+});
+
+export default store;
